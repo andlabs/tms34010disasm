3b14788b89992ba7fa06f189c8779c32064e7fba
Pietro Gagliardi <pietro10@mac.com>
2013-03-18 23:05:26 -0400

    Started filling up the opcode table.

--- opcodetbl.go
+++ opcodetbl.go
@@ -5,14 +5,35 @@ import (
 	"fmt"
 )
 
+// TODO type opfunc
+
 type Opcode struct {
 	Expected		uint16
 	Mask		uint16
-	// TODO Func
+	Func			opfunc
 }
 
 var opcodes = []Opcode{
-	// ...
+	op(o_abs,		0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0),
+	op(o_add,		0, 1, 0, 0, 0, 0, 0),
+	op(o_addc,	0, 1, 0, 0, 0, 0, 1),
+	op(o_addiw,	0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0),
+	op(o_addil,	0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1),
+	op(o_addk,	0, 0, 0, 1, 0, 0),
+	op(o_addxy,	1, 1, 1, 0, 0, 0, 0),
+	op(o_and,		0, 1, 0, 1, 0, 0, 0),
+	// andi is an alias for andni; TODO provide an option?
+	op(o_andn,	0, 1, 0, 1, 0, 0, 1),
+	op(o_andni,	0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0),
+	op(o_btst,		0, 0, 0, 1, 1, 1),				// btst immediate,register
+	op(o_btst,		0, 1, 0, 0, 1, 0, 1),			// btst register,register
+	op(o_call,		0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1),
+	op(o_calla,	0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1),
+	op(o_callr,	0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1),
+	op(o_clr,		0, 1, 0, 1, 0, 1, 1),
+	op(o_clrc,		0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0),
+	op(o_cmp,	0, 1, 0, 0, 1, 0, 0),
+	op(o_cmpiw,	0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0),
 }
 
 func ui16(bits ...uint16) uint16 {
@@ -30,12 +51,12 @@ func ui16(bits ...uint16) uint16 {
 	return x
 }
 
-func op(bits ...uint16) (o Opcode) {
+func op(func opfunc, bits ...uint16) (o Opcode) {
 	o.Expected = ui16(bits...)
 	for i := 0; i < len(bits); i++ {
 		bits[i] = 1		// TODO whether or not this should be legal should be in the spec
 	}
 	o.Mask = ui16(bits...)
-	// TODO func
+	o.Func = func
 	return o
 }
